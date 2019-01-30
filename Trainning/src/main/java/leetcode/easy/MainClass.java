7c8decc5e175e918e122cbd48103b0df4be68590
huqiqi@vpgame.cn <zhangligang@vpgame.cn>
2019-01-30 18:42:12 +0800

    新的

--- Trainning/src/main/java/leetcode/easy/MainClass.java
+++ Trainning/src/main/java/leetcode/easy/MainClass.java
@@ -6,24 +6,72 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.SocketOptions;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import static jdk.nashorn.internal.objects.Global.print;
 
 public class MainClass {
-    //排序测试
-    public static void main(String[] args) {
-        int[] data = new int[] { 5, 3, 6, 2, 1, 9, 4, 8, 7 };
-        Sort.bubbleSort(data);
-        System.out.println("排序后的数组：");
-        print(data);
-    }
-
-    public static void print(int[] data) {
-        for (int i = 0; i < data.length; i++) {
-            System.out.print(data[i] + "\t");
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
         }
-        System.out.println();
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
     }
 
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            boolean ret = new Solution101_Temp().isSymmetric(root);
+
+            String out = booleanToString(ret);
+
+            System.out.print(out);
+        }
+    }
 
 }
--- Trainning/src/main/java/leetcode/easy/Solution88.java
+++ Trainning/src/main/java/leetcode/easy/Solution88.java
@@ -2,7 +2,9 @@ package leetcode.easy;
 
 public class Solution88 {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
-
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Sort.MergeSort(nums1);
     }
 }
